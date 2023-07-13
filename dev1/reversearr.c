e20af057fa9cd08aaf51d4a4cf6bf95d3c928a91
pratik <pratikpatil1615@gmail.com>
2023-07-14 00:35:19 +0530

    first commit

---
+++ dev1/reversearr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+
+int a[50],i,n,z;
+
+printf("Enter a arrray limit\n");
+scanf("%d",&n);
+
+printf("Enter a elemnt in the array");
+for(i=0;i<n;i++){
+
+    scanf("%d",&a[i]);
+}
+printf("Dispaly element in the array");
+for(i=n-1;i>=0;i--){
+
+    printf("%d\t",a[i]);
+}
+
+}
+exit(0);
